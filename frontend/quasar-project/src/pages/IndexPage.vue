8f82a85dfe4ec44b9d0290efb184f73655a22c3b
kbazon <karenbazon20@gmail.com>
2024-10-31 23:55:32 +0100

    Kreiranje Quasar projekta

---
+++ frontend/quasar-project/src/pages/IndexPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-page class="flex flex-center">
+    <img
+      alt="Knjiznica slika"
+      src="~assets/Daco_4059314.png"
+      style="width: 300px; height: 300px"
+    />
+  </q-page>
+</template>
+
+<script setup>
+defineOptions({
+  name: "IndexPage",
+});
+</script>
---
+++ frontend/quasar-project/src/router/routes.js
@@ -0,0 +1,42 @@
+const routes = [
+  {
+    path: "/",
+    component: () => import("layouts/MainLayout.vue"),
+    children: [
+      { path: "/", component: () => import("pages/IndexPage.vue") },
+      {
+        path: "/popis_Knjiga",
+        component: () => import("pages/PopisKnjigaPage.vue"),
+      },
+      {
+        path: "/o_nama",
+        component: () => import("pages/OnamaPage.vue"),
+      },
+      {
+        path: "/pretrazivanje",
+        component: () => import("pages/PretrazivanjePage.vue"),
+      },
+      {
+        path: "/lokacija",
+        component: () => import("pages/LokacijaPage.vue"),
+      },
+      {
+        path: "/login",
+        component: () => import("pages/LoginPage.vue"),
+      },
+      {
+        path: "/registracija",
+        component: () => import("pages/RegistracijaPage.vue"),
+      },
+    ],
+  },
+
+  // Always leave this as last one,
+  // but you can also remove it
+  {
+    path: "/:catchAll(.*)*",
+    component: () => import("pages/ErrorNotFound.vue"),
+  },
+];
+
+export default routes;
